a3b621b019c77947c74133d95e0f27a08d28549d
tillenotuoma <totuoma27@gmail.com>
2025-03-12 20:25:09 +0300

    added migrations and seeded data

--- lib/seed.py
+++ lib/seed.py
@@ -1,20 +1,23 @@
 from faker import Faker
 import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Student, Course
 
 fake = Faker()
 
-def generate_sample_data(num_records):
-    data = []
-    for _ in range(num_records):
-        record = {
-            "name": fake.name(),
-            "email": fake.email(),
-            "age": random.randint(18, 35),
-        }
-        data.append(record)
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///students_courses.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-if __name__ == "__main__":
-    sample_data = generate_sample_data(100)
-    for record in sample_data:
-        print(record)
+    for _ in range(15):
+        course = Course.create(session, fake.sentence())
+        for _ in range(random.randint(1, 5)):
+            Student.create(session, fake.name(), course.id)
+
+    session.close()
+
+    
     
--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,4 @@
+from lib.models import Base
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -18,7 +19,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
